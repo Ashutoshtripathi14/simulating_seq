641aeb437c8a50303208e47d1f063ce0f5391197
Ashutoshtripathi14 <at943@cam.ac.uk>
2022-07-15 10:38:15 +0100

    Add new files

--- to_run.sh
+++ to_run.sh
@@ -8,4 +8,8 @@
 #BSUB -n 1
 #BSUB -M 1GB
 
+module load python-3.9.0-gcc-9.3.0-5t75egs 
+module load py-numpy-1.19.4-gcc-9.3.0-x2neh6p 
+module load py-pandas-1.1.5-gcc-9.3.0-rjsya74
 
+python ~/LSFexample/simulating_seq/script_to_sim.py 
